c5420441faa70fc65f7bd7d7a525a8704d1d2801
Naomi De Wys <146399253+naomidewys@users.noreply.github.com>
2024-10-21 17:09:48 +1000

    Update Maze.py
    Reattempted reeborg's maze and added comments for clarity.

--- Maze (1).py
+++ Maze (1).py
@@ -1,21 +1,23 @@
+# defines right turn because only turn left and move forward are pre-defined in program.
 def turn_right():
     turn_left()
     turn_left()
     turn_left()
 
+# reeborg moves forward until reaches a wall
 while front_is_clear():
     move()
 turn_left()
-    
-while at_goal() != True:
+
+# while loop until goal is reached
+while not at_goal():
+    # reeborg follows right side of maze
     if right_is_clear():
         turn_right()
         move()
+    # if wall on right side but front is clear, move forward    
     elif front_is_clear():
         move()
+    # if wall in front and on right, turn left until front is clear again to restart first loop
     else:
         turn_left()
-################################################################
-# WARNING: Do not change this comment.
-# Library Code is below.
-################################################################
